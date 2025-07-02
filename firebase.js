83b89cd58eda12b80db05e6798250e2f8f75143d
donnuj <antony.jasper@gmail.com>
2025-07-02 18:03:38 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Firebase config + init
+// firebase.js
+const { initializeApp } = firebase;
+const { getFirestore } = firebase;
+const { getAuth } = firebase;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAL7-FJENLKptEN372lwt5GCo102uIEn4Y",
+  authDomain: "modulo-herois.firebaseapp.com",
+  projectId: "modulo-herois",
+  storageBucket: "modulo-herois.firebasestorage.app",
+  messagingSenderId: "861034134955",
+  appId: "1:861034134955:web:31eebd05af9d17e0ee66ee"
+};
